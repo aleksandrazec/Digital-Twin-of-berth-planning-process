2f469c1a0be52fcbbc4b34c82d1238606e91cbc9
Aleksandra Zec <sanjazec04@gmail.com>
2025-05-17 13:02:21 +0200

    initial commit

---
+++ data scrape.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import xml.etree.ElementTree as ET
+import requests
+import os
+
+output_dir = "vessel_info"
+os.makedirs(output_dir, exist_ok=True)
+
+urls = [
+    "https://www.mardep.gov.hk/e_files/en/pub_services/report_day0/RP05005.XML",
+    "https://www.mardep.gov.hk/e_files/en/pub_services/report_day0/RP04005.XML",
+    "https://www.mardep.gov.hk/e_files/en/pub_services/report_day0/RP04505.XML",
+    "https://www.mardep.gov.hk/e_files/en/pub_services/report_day0/RP05505.XML",
+]
+
+counter=0
+
+for i, url in enumerate(urls):
+    try:
+        if i%4==0:
+            counter=counter+1
+
+        response = requests.get(url)
+        response.raise_for_status()
+        
+        root = ET.fromstring(response.content)
+
+        data = []
+        for vessel in root.iter('G_SQL1'):
+            record = {child.tag: child.text for child in vessel}
+            data.append(record)
+        
+        df = pd.DataFrame(data)
+
+        if "ATA_TIME" in df.columns:
+            filename = f"ATA{counter}.csv"
+        elif "ATD_TIME" in df.columns:
+            filename = f"ATD{counter}.csv"
+        elif "ETA_TIME" in df.columns:
+            filename = f"ETA{counter}.csv"
+        elif "ETD_TIME" in df.columns:
+            filename = f"ETD{counter}.csv"
+
+        output_path = os.path.join(output_dir, filename)
+
+        df.to_csv(output_path, index=False)
+
+        print("Data saved to "+filename)
+
+    except Exception as e:
+        print(f"Failed to process {url}: {e}")
