00ea2d1bd500efb1463ee44ed0b1b8eb8b2aafc5
Filip Trajkoski <89231028@student.upr.si>
2025-05-18 02:47:36 +0200

    Working ML model, just need to generate more data and train it

--- ml_model/data.py
+++ ml_model/data.py
@@ -2,17 +2,35 @@ import pandas as pd
 import numpy as np
 import torch
 
+import sys
+sys.path.append('../data processing/')
+from get_data import get_data 
+
+
+
 # TODO - change the data
 # the data needs to have beside vessel attributes, weather, congestion, reliabality
 # a couple of different ATAs, ATDs
 # 
 
 def load_data(filepath="your_combined_dataset.csv"):
-    data = pd.read_csv(filepath)  # Replace with your actual file path
+    # data = pd.read_csv(filepath)  # Replace with your actual file path
 
-    feature_cols = ['Type', 'Size', 'Draft', 'ETA', 'ETD', 'Congestion', 
-                    'Weather', 'Reliability', 'Effectiveness', 'Work_Environment', 'Priority_Of_Shipment']
-    label_cols = ['ATA', 'ATD', 'Berth_No']
+    # feature_cols = ['Type', 'Size', 'Draft', 'ETA', 'ETD', 'Congestion', 
+    #                 'Weather', 'Reliability', 'Effectiveness', 'Work_Environment', 'Priority_Of_Shipment']
+    # label_cols = ['ATA', 'ATD', 'Berth_No']
+
+    
+
+    data, feature_cols, label_cols = get_data() 
+    
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.max_rows', None)
+
+    print(data[feature_cols])
+    print(data[label_cols])
+
+    
 
     X = data[feature_cols].values.astype(np.float32)
     y = data[label_cols].values.astype(np.float32)
